1fab6b257ce1f386076fef6f910e89e9080f2774
dinomoon <ansrud45@gmail.com>
2022-01-31 13:34:42 +0900

    etc: 백엔드 개발 환경 설정 controller 수정

--- backend/src/controllers/post.js
+++ backend/src/controllers/post.js
@@ -1,42 +1,32 @@
 exports.getPost = (req, res) => {
-  res.json({
-    success: req.params,
-  });
+  res.status(200).json({ success: '특정 포스트' });
 };
 
 exports.getPosts = (req, res) => {
-  console.log(req.query);
-  res.json({
-    success: 'hello',
-  });
+  const queryObj = req.query;
+  if (Object.keys(queryObj).length === 0) {
+    res.status(200).json({ success: '포스트 목록' });
+    return;
+  }
+  res.status(200).json({ success: '포스트 필터링' });
 };
 
 exports.registerPost = (req, res) => {
-  res.json({
-    success: req.body,
-  });
+  res.status(200).json({ success: '포스트 등록' });
 };
 
 exports.editPost = (req, res) => {
-  res.json({
-    success: 'hello',
-  });
+  res.status(200).json({ success: '포스트 수정' });
 };
 
 exports.deletePost = (req, res) => {
-  res.json({
-    success: 'hello',
-  });
+  res.status(200).json({ success: '포스트 삭제' });
 };
 
 exports.addLikes = (req, res) => {
-  res.json({
-    success: '관심 등록',
-  });
+  res.status(200).json({ success: '관심 등록' });
 };
 
 exports.deleteLikes = (req, res) => {
-  res.json({
-    success: '관심 해제',
-  });
+  res.status(200).json({ success: '관심 해제' });
 };
