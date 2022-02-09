312e3d2490ee93aace3967516f83b151227dc815
y00n-lee <0613myday@gmail.com>
2022-02-09 15:02:45 +0900

    feat: 상세페이지의 Card컴포넌트 작성완료 (#34)

--- frontend/src/components/Card/Card.jsx
+++ frontend/src/components/Card/Card.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import styles from './Card.module.css';
 import Button from '../Button/Button';
-import contactIcon from './images/contact_calender.svg';
-import exploreIcon from './images/explore.svg';
-import heartIcon from './images/heart.svg';
+import contact from './images/contact_calender.svg';
+import explore from './images/explore.svg';
+import heartRed from './images/heart_red.svg';
+import heartGray from './images/heart_gray.svg';
 
-const Card = ({ ...props }) => {
-  if (props.cardType === 'create-card')
+const Card = ({ cardType, style, content }) => {
+  const {
+    like,
+    pic,
+    likes,
+    loc,
+    tags,
+    memCount,
+    image,
+    title,
+    subTitle,
+    name,
+    profile,
+  } = content;
+  if (cardType === 'create-card')
     return (
-      <div className={`${styles['card']} ${styles['create-card']}`}>
+      <div
+        style={style}
+        className={`${styles['card']} ${styles['create-card']}`}
+      >
         <Button
           width='10rem'
           height='10rem'
@@ -16,43 +33,41 @@ const Card = ({ ...props }) => {
           bg='#7EDA8B'
           text='+'
           ftsize='5rem'
-        ></Button>
+        />
         <span className={styles['create-card-span']}>새로운 모임 만들기</span>
       </div>
     );
 
-  if (props.cardType === 'detail-card')
+  if (cardType === 'detail-card')
     return (
-      <div className={`${styles['card']} ${styles['detail-card']}`}>
+      <div
+        style={style}
+        className={`${styles['card']} ${styles['detail-card']}`}
+      >
         <div className={styles['detail-tags']}>
-          {props.tags.map((tag) => {
+          {tags.map((tag) => {
             const hashTag = `#${tag}`;
             return (
               <Button
                 height='3rem'
                 radius='25px'
-                ftsize='1rem'
+                ftsize='1.2rem'
                 text={hashTag}
-                bg='F3F5F8'
-                color='666666'
-                style={{
-                  flexBasis: 'content',
-                  padding: '0 1rem',
-                  margin: '0.2rem 0.5rem',
-                }}
-              ></Button>
+                bg='#F3F5F8'
+                color='#666666'
+              />
             );
           })}
         </div>
         <div className={styles['detail-text-div']}>
           <div className={styles['detail-text']}>
-            <img src={contactIcon} />
-            <span>{'33'}명</span>
+            <img src={contact} />
+            <span>{memCount}명</span>
           </div>
 
           <div className={styles['detail-text']}>
-            <img src={exploreIcon} />
-            <span>{'위치'}</span>
+            <img src={explore} />
+            <span>{loc}</span>
           </div>
         </div>
         <div className={styles['detail-buttons-middle']}>
@@ -64,20 +79,26 @@ const Card = ({ ...props }) => {
             text='모집중'
             radius='140px'
             bg='#ffffff'
-            flexBasis='center'
-          ></Button>
+          />
           <Button
             width='26rem'
             height='6rem'
             color='#666666'
             text='참가 신청'
             radius='140px'
-            flexBasis='center'
             bg='#B2F2BB'
-          ></Button>
+          />
         </div>
+
         <div className={styles['detail-buttons-bottom']}>
-          <img style={{ width: '2rem', height: '2rem' }} />
+          <div className={styles['likes-people']}>
+            <Button width='9rem' height='3rem' bg='#ffffff' cursor='default'>
+              {pic.map((p, idx) => {
+                if (idx < 3) return <img src={p} />;
+              })}
+            </Button>
+          </div>
+
           <Button
             width='10rem'
             height='4.6rem'
@@ -86,18 +107,63 @@ const Card = ({ ...props }) => {
             radius='140px'
             flexBasis='center'
             bg='#ffffff'
-            text={'likes'}
-            image={heartIcon}
-            ftsize='1.2rem'
-            style={{
-              flexBasis: 'content',
-            }}
-          ></Button>
+            text={likes}
+            ftsize='1.6rem'
+          >
+            {like === true ? <img src={heartRed} /> : <img src={heartGray} />}
+          </Button>
         </div>
       </div>
     );
 
-  if (props.cardType === 'recruit-card')
-    return <div className={styles[props.cardType]}></div>;
+  if (cardType === 'recruit-card')
+    return (
+      <div
+        style={style}
+        className={`${styles['card']} ${styles['recruit-card']}`}
+      >
+        <img src={image} className={styles['recruit-img']} />
+        <div className={styles['recruit-text']}>
+          <div className={styles['recruit-tags']}>
+            tags
+            {
+              <Button
+                height='1.7rem'
+                radius='25px'
+                ftsize='1.2rem'
+                text='ssss'
+                bg='#ffffff'
+                color='#7EDA8B'
+                border='#7EDA8B solid 1px'
+                style={{
+                  flexBasis: 'content',
+                  padding: '1rem',
+                }}
+              />
+            }
+          </div>
+          <span className={styles['recruit-title']}>{title}</span>
+          <span className={styles['recruit-subtitle']}>{subTitle}</span>
+        </div>
+      </div>
+    );
 };
+
+Card.defaultProps = {
+  cardType: 'create',
+  content: {
+    like: false,
+    pic: [],
+    likes: 0,
+    loc: '위치 없음',
+    tags: [],
+    memCount: 0,
+    image: '',
+    title: '',
+    subTitle: '',
+    name: '',
+    profile: '',
+  },
+};
+
 export default Card;
